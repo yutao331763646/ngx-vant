a8fc76e371c101f99ed3b97282c7066592296cd4
yutao331763646 <yutao0818@vip.qq.com>
2021-01-19 17:07:29 +0800

    feat(layout):加入col组件

--- projects/ngx-vant/col/col.component.ts
+++ projects/ngx-vant/col/col.component.ts
@@ -1,16 +1,56 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Host, Input, OnChanges, OnInit, Renderer2, SimpleChanges } from '@angular/core';
+import { removeNgTag } from '../utils';
+export function isNotNil<T>(value: T): value is NonNullable<T> {
+    return typeof value !== 'undefined' && value !== null;
+}
 @Component({
-    selector: 'app-col',
+    selector: 'van-col',
     templateUrl: './col.component.html',
     styleUrls: ['./col.component.less']
 })
-export class ColComponent implements OnInit {
-    @Input() span: number | string = 1
-    @Input() offset: number | string = 1
-    constructor() { }
+export class ColComponent implements OnInit, OnChanges {
+    classMap: { [key: string]: boolean } = {};
+    @Input() span: number | string = '';
+    @Input() offset: number | string = '';
+    paddingLeft: string | null = ''
+    paddingRight: string | null = ''
+    constructor(
+        private elementRef: ElementRef,
+        public renderer: Renderer2,
+    ) { }
 
     ngOnInit() {
+        removeNgTag(this.elementRef.nativeElement)
+        this.setHostClassMap();
     }
-
+    ngOnChanges(): void {
+        this.setHostClassMap();
+    }
+    setGutter(space: any) {
+        console.log(space)
+        if (space) {
+            const { left, right } = space;
+            console.log(left, right)
+            this.paddingLeft = left ? `${left}px` : null
+            this.paddingRight = right ? `${right}px` : null
+        }
+    }
+    setHostClassMap(): void {
+        const hostClassMap = {
+            ['van-col']: true,
+            [`van-col--${this.span}`]: isNotNil(this.span),
+            [`van-col--offset-${this.offset}`]: isNotNil(this.offset),
+        };
+        // for (const i in this.classMap) {
+        //     if (this.classMap.hasOwnProperty(i)) {
+        //         this.renderer.removeClass(this.elementRef.nativeElement, i);
+        //     }
+        // }
+        this.classMap = { ...hostClassMap };
+        // for (const i in this.classMap) {
+        //     if (this.classMap.hasOwnProperty(i) && this.classMap[i]) {
+        //         this.renderer.addClass(this.elementRef.nativeElement, i);
+        //     }
+        // }
+    }
 }
--- projects/ngx-vant/row/row.component.ts
+++ projects/ngx-vant/row/row.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, EventEmitter, Input, OnInit, Output, QueryList } from '@angular/core';
+import { ColComponent } from '../col/col.component';
+import { removeNgTag } from '../utils';
 
 export type RowType = 'flex' | ''
 export type RowAlign = 'top' | 'center' | 'bottom';
@@ -10,18 +12,58 @@ export type RowJustify =
     | 'space-between';
 
 @Component({
-    selector: 'app-row',
+    selector: 'van-row',
     templateUrl: './row.component.html',
     styleUrls: ['./row.component.less']
 })
-export class RowComponent {
+export class RowComponent implements OnInit, AfterContentInit {
+    @ContentChildren(ColComponent)
+    clos: QueryList<ColComponent> | undefined;
     @Input() type: RowType = ''
     @Input() justify: RowJustify = 'start'
     @Input() align: RowAlign = 'top'
     @Input() gutter: string | number = ''
     @Output() readonly click = new EventEmitter<MouseEvent>();
-    constructor() { }
+    constructor(private el: ElementRef) { }
     onClick(e: MouseEvent): void {
         this.click.emit(e);
+    }
+    ngOnInit() {
+        removeNgTag(this.el.nativeElement)
+    }
+    ngAfterContentInit() {
+        console.log(this.clos);
+        const gutter = Number(this.gutter);
+        const spaces: any = [];
+        const groups: number[][] = [[]];
+
+        let totalSpan = 0;
+        this.clos?.forEach((item, index) => {
+            totalSpan += Number(item.span);
+
+            if (totalSpan > 24) {
+                groups.push([index]);
+                totalSpan -= 24;
+            } else {
+                groups[groups.length - 1].push(index);
+            }
+        })
+        groups.forEach((group: number[]) => {
+            const averagePadding = (gutter * (group.length - 1)) / group.length;
+
+            group.forEach((item: number, index: number) => {
+                if (index === 0) {
+                    spaces.push({ right: averagePadding });
+                } else {
+                    const left = gutter - spaces[item - 1].right;
+                    const right = averagePadding - left;
+                    spaces.push({ left, right });
+                }
+            });
+        });
+        this.clos?.forEach((item, index) => {
+            item.setGutter(spaces[index])
+        })
+        console.log(spaces)
     }
 }
