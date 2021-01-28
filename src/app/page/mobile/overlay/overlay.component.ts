1eb4844de74a3d47402e527ca16c778b857ebcb9
yutao331763646 <yutao0818@vip.qq.com>
2021-01-28 23:42:29 +0800

    feat(demo:overlay):overlay示例已完成

--- src/app/page/mobile/overlay/overlay.component.ts
+++ src/app/page/mobile/overlay/overlay.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-overlay',
   templateUrl: './overlay.component.html',
-  styleUrls: ['./overlay.component.scss']
+  styleUrls: ['./overlay.component.less']
 })
 export class OverlayComponent implements OnInit {
     show:boolean=false
+    show2:boolean=false
   constructor() { }
 
   ngOnInit() {
