3a78d478c95eb57b051a2af14b503a13133f1d44
yutao331763646 <yutao0818@vip.qq.com>
2021-02-01 16:55:14 +0800

    feat(tabs):tabs init

---
+++ projects/ngx-vant/tab/tab.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabComponent } from './tab.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TabComponent]
+})
+export class TabModule { }
---
+++ projects/ngx-vant/tabs/tabs.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabsComponent } from './tabs.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TabsComponent]
+})
+export class TabsModule { }
