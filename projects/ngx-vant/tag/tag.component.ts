380ea1023c20e08654aa144c8b69371a84e9b8c2
yutao331763646 <yutao0818@vip.qq.com>
2021-02-09 21:34:28 +0800

    feat(sticky):sticky init

--- projects/ngx-vant/tag/tag.component.ts
+++ projects/ngx-vant/tag/tag.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+export type TagType = 'default' | 'primary' | 'success' | 'danger';
+export type TagSize = 'large' | 'medium';
+export type TagProps = {
+  type: TagType;
+  size?: TagSize;
+  mark?: boolean;
+  color?: string;
+  plain?: boolean;
+  round?: boolean;
+  textColor?: string;
+  closeable?: boolean;
+};
 @Component({
   selector: 'van-tag',
   templateUrl: './tag.component.html',
