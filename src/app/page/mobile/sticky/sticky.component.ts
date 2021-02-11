2849e25ca3cc5d7887b8541a3c5492f957374d3f
yutao331763646 <yutao0818@vip.qq.com>
2021-02-11 23:30:59 +0800

    feat(sticky):sticky demo init

---
+++ src/app/page/mobile/sticky/sticky.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sticky',
+  templateUrl: './sticky.component.html',
+  styleUrls: ['./sticky.component.less']
+})
+export class StickyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
