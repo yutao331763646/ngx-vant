72e506e4a399d37ab41624096ce7ea21d909165d
yutao331763646 <yutao0818@vip.qq.com>
2021-02-22 18:01:06 +0800

    feat(tag):tag 100%

--- projects/ngx-vant/tag/tag.component.ts
+++ projects/ngx-vant/tag/tag.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { overlayAnimation } from '../overlay/overlay.animation';
 export type TagType = 'default' | 'primary' | 'success' | 'danger' | 'warning';
 export type TagSize = 'large' | 'medium' | '';
 @Component({
     selector: 'van-tag',
     templateUrl: './tag.component.html',
-    styleUrls: ['./tag.component.less']
+    styleUrls: ['./tag.component.less'],
+    animations: [overlayAnimation]
 })
 export class TagComponent implements OnInit {
     @Input() type: TagType = 'default'
@@ -15,9 +17,18 @@ export class TagComponent implements OnInit {
     @Input() mark: boolean = false
     @Input() textColor: string = ''
     @Input() closeable: boolean = false
+    style: { [klass: string]: any } = {}
+    show: boolean = true;
     constructor() { }
 
     ngOnInit() {
+        console.log(this.textColor)
+        const key = this.plain ? 'color' : 'backgroundColor';
+        this.style = { [key]: this.color };
     }
-
+    close(event: MouseEvent) {
+        event.stopPropagation();
+        console.log(event)
+        this.show = false
+    }
 }
--- projects/ngx-vant/tag/tag.module.ts
+++ projects/ngx-vant/tag/tag.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TagComponent } from './tag.component';
+import { IconModule } from '../icon/icon.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  exports:[TagComponent],
-  declarations: [TagComponent]
+    imports: [
+        CommonModule, IconModule
+    ],
+    exports: [TagComponent],
+    declarations: [TagComponent]
 })
 export class TagModule { }
