d8a7e99a5771ab94d1b5b424a268374f0fd861ca
yutao <yutao0818@vip.qq.com>
2021-07-27 15:46:19 +0800

    feat(tag):notify doc  doing

--- src/app/page/basic/notify/notify.component.ts
+++ src/app/page/basic/notify/notify.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
-  selector: 'app-notify',
-  templateUrl: './notify.component.html',
-  styleUrls: ['./notify.component.less']
+    selector: 'app-notify',
+    templateUrl: './notify.component.html',
+    styleUrls: ['./notify.component.less']
 })
 export class NotifyComponent implements OnInit {
+    private _readMe: HTMLElement | string = '';
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.setReadMe()
+    }
+    setReadMe() {
+        this.readMe = require(`!html-loader!markdown-loader!./README.zh-CN.md`).default;
+    }
 
 }
