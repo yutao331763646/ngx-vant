ad8c3fc45e752ed5635476048f22219cbcf54406
yutao <yutao0818@vip.qq.com>
2021-07-31 13:38:19 +0800

    feat(icon doc):icon doc done

--- src/app/page/basic/icon/icon.component.ts
+++ src/app/page/basic/icon/icon.component.ts
@@ -2,9 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
-  selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss']
+    selector: 'app-icon',
+    templateUrl: './icon.component.html',
+    styleUrls: ['./icon.component.scss']
 })
 export class IconComponent implements OnInit {
 
@@ -16,13 +16,13 @@ export class IconComponent implements OnInit {
         return cardWrapper(this._readMe);
     }
 
-  
-  constructor( ) { }
 
-  ngOnInit() {
-    this.setReadMe()
-}
-setReadMe() {
-    this.readMe = require(`!html-loader!markdown-loader!./README.zh-CN.md`).default;
-}
+    constructor() { }
+
+    ngOnInit() {
+        this.setReadMe()
+    }
+    setReadMe() {
+        this.readMe = require(`!html-loader!markdown-loader!./README.zh-CN.md`).default;
+    }
 }
