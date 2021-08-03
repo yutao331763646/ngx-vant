9b5ef75f5b3da1ea4763187414c7850a52ec4ac0
yutao <yutao0818@vip.qq.com>
2021-08-03 15:00:24 +0800

    feat(theme):add theme img

--- src/app/page/basic/theme/theme.component.ts
+++ src/app/page/basic/theme/theme.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
-    selector: 'app-theme',
+    selector: 'van-theme',
     templateUrl: './theme.component.html',
     styleUrls: ['./theme.component.less']
 })
