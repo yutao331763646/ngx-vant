5d15c666d919cf8c61582328b49cadde66b84019
yutao <yutao0818@vip.qq.com>
2021-08-03 13:51:33 +0800

    feat(style):修改less引入方式，以实现自定义主题

--- projects/ngx-vant/cell-group/cell-group.component.ts
+++ projects/ngx-vant/cell-group/cell-group.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
     selector: 'van-cell-group',
     templateUrl: './cell-group.component.html',
-    // styleUrls: ['./cell-group.component.less']
 })
 export class CellGroupComponent implements OnInit {
     @Input() title: string = '';
