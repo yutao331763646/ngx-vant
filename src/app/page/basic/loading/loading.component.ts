1ed13620c4a6fcc23da18a28254ba6db5d09ec31
yutao <yutao0818@vip.qq.com>
2021-08-08 12:12:03 +0800

    feat(loading doc):loading doc doing

--- src/app/page/basic/loading/loading.component.ts
+++ src/app/page/basic/loading/loading.component.ts
@@ -15,21 +15,7 @@ export class LoadingComponent implements OnInit {
     get readMe() {
         return cardWrapper(this._readMe);
     }
-    cards = [{
-        title: '介绍',
-        desc: '加载图标，用于表示加载中的过渡状态。'
-
-    }, {
-        title: '引入',
-        code:
-            `  import LoadingModule from 'VantModule'; `
-    }]
-    cards2 = [{
-        title: '按钮类型',
-        desc: this.sanitizer.bypassSecurityTrustHtml('通过 <code>type</code> 属性可以设置加载图标的类型，默认为 <code>circular</code>，可选值为 <code>spinner</code>。'),
-        code: `  <van-loading />
-  <van-loading type="spinner" /> `
-    }]
+   
     constructor(private sanitizer: DomSanitizer) { }
 
 
