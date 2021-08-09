8d6ab79a5fb719113fa3e8a1d8f6a60e59c05b07
yutao <yutao0818@vip.qq.com>
2021-08-09 20:32:02 +0800

    feat(i18n):i18n doing

---
+++ library/ngx-vant/i18n/public-api.ts
@@ -0,0 +1,8 @@
+
+export * from './i18n.directive'
+export * from './i18n.module'
+export * from './i18n.pipe'
+export * from './i18n.prop'
+export * from './i18n.service'
+
+export { default as zh_CN } from './languages/zh_CN';
