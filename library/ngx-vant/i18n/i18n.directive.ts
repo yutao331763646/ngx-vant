a86a2e311bc07e9b5068e38f0b69fb9ddc090ee4
yutao <yutao0818@vip.qq.com>
2021-08-09 20:28:06 +0800

    feat(i18n):i18n doing

--- library/ngx-vant/i18n/i18n.directive.ts
+++ library/ngx-vant/i18n/i18n.directive.ts
@@ -1,16 +1,27 @@
-import { Directive, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
-
+import { Directive, ElementRef, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Subject } from 'rxjs';
+import { VantI18nService } from './i18n.service';
+import { takeUntil } from 'rxjs/operators';
 @Directive({
     selector: '[vant-i18n]'
 })
 export class VantI18nDirective implements OnChanges, OnDestroy {
+    @Input('x-i18n') path!: string;
+    private _unSubject = new Subject<void>();
+    constructor(private locale: VantI18nService, private elementRef: ElementRef) {
+        this.locale.localeChange.pipe(takeUntil(this._unSubject)).subscribe(() => this.setLocale());
+    }
 
-    constructor() { }
     ngOnDestroy(): void {
         throw new Error('Method not implemented.');
     }
     ngOnChanges(changes: SimpleChanges): void {
         throw new Error('Method not implemented.');
     }
-
+    setLocale() {
+        if (typeof this.path !== 'undefined') {
+            const content = this.locale.translate(this.path);
+            this.elementRef.nativeElement.innerText = content;
+        }
+    }
 }
