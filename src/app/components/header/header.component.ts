4aba4e699339fddfeb07650c166c8746e9f4c902
yutao <yutao0818@vip.qq.com>
2021-08-09 20:56:06 +0800

    feat(i18n):i18n doing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'vant-doc-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+    selector: 'vant-doc-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+    lang = 'En'
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
+    onSwitchLang() {
 
-  ngOnInit() {
-  }
-
+    }
 }
