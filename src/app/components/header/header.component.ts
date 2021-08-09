94223c5648ff4a52da8e703eb296f9081ccb3d18
yutao <yutao0818@vip.qq.com>
2021-08-09 20:59:55 +0800

    feat(i18n):i18n doing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { en_US, VantI18nService, zh_CN } from 'ngx-vant/i18n';
 
 @Component({
     selector: 'vant-doc-header',
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     lang = 'En'
-    constructor() { }
+    constructor(private vantI18n: VantI18nService) { }
 
     ngOnInit() {
     }
     onSwitchLang() {
+
+        this.lang = this.lang === 'En' ? '中文' : 'En'
+        const lang = this.lang === 'En' ? en_US : zh_CN
+        this.vantI18n.setLocale(lang);
 
     }
 }
