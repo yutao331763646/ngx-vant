996b3db9c63fe19b54958b4ea5c99bb5677a19d8
yutao <yutao0818@vip.qq.com>
2021-08-11 01:20:52 +0800

    feat(i18n):i18n doing

--- src/app/page/mobile/button/button.component.ts
+++ src/app/page/mobile/button/button.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { en_US, VantI18nService, zh_CN } from 'ngx-vant/i18n';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-button',
@@ -10,31 +9,23 @@ import { takeUntil } from 'rxjs/operators';
     styleUrls: ['./button.component.less']
 })
 export class ButtonComponent implements OnInit {
-    private _unSubject = new Subject<void>();
     constructor(
         private router: Router,
         private vantI18n: VantI18nService,
     ) {
         this.router.events.subscribe((event: any) => {
-            // console.log(event)
             if (event instanceof NavigationEnd) {
-                // const host = window.location.href.split('/#/')
-                // console.log(host[1].split('/')[0])
-                // const lang = host[1].split('/')[0] !== 'en_US' ? en_US : zh_CN
-                // this.vantI18n.setLocale(lang);
+                const host = window.location.href.split('/#/')
+                console.log(host[1].split('/')[0])
+                const lang = host[1].split('/')[0] !== 'en_US' ? en_US : zh_CN
+                this.vantI18n.setLocale(lang);
             }
         })
     }
 
     ngOnInit() {
-        this.vantI18n.localeChange.pipe(
-            takeUntil(this._unSubject)
-        ).subscribe(res => {
-            console.log(res)
-        })
+        
     }
     ngOnDestroy() {
-        this._unSubject.next();
-        this._unSubject.unsubscribe();
     }
 }
--- src/app/page/mobile/mobile.module.ts
+++ src/app/page/mobile/mobile.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { VANT_I18N, zh_CN ,en_US, Vant18nModule} from "ngx-vant/i18n";
 @NgModule({
     imports: [
-        RouterModule, CommonModule,Vant18nModule
+        RouterModule, CommonModule
     ],
 
-    // providers: [{ provide: VANT_I18N, useValue: en_US }],
 
 })
 export class MobileModule { }
