b0ac6892b678693d05a1139f3fdfa7437c4429a9
yutao <yutao0818@vip.qq.com>
2021-08-12 13:56:18 +0800

    feat(button en doc):button en doc doing

---
+++ src/app/common/i18n/languages/en_US.ts
@@ -0,0 +1,9 @@
+export default {
+    locale: 'en_US',
+    button: {
+        comments: 'Comments',
+        giveALike: 'Like',
+        reply: 'Reply',
+        more: 'See more',
+    }
+}
---
+++ src/app/common/i18n/languages/zh_CN.ts
@@ -0,0 +1,10 @@
+export default {
+    locale: 'zh_CN',
+    button: {
+        primary: '主要按钮',
+        info: '点赞',
+        default: '回复',
+        danger: '查看更多',
+        warning: '查看更多',
+      },
+}
