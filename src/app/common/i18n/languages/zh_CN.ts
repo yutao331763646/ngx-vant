af8905e53077fa5ea35cd968b18c9156afffcfa8
yutao <yutao0818@vip.qq.com>
2021-08-12 13:56:38 +0800

    feat(button en doc):button en doc doing

--- src/app/common/i18n/languages/zh_CN.ts
+++ src/app/common/i18n/languages/zh_CN.ts
@@ -2,9 +2,9 @@ export default {
     locale: 'zh_CN',
     button: {
         primary: '主要按钮',
-        info: '点赞',
-        default: '回复',
-        danger: '查看更多',
-        warning: '查看更多',
-      },
+        info: '信息按钮',
+        default: '默认按钮',
+        danger: '危险按钮',
+        warning: '警告按钮',
+    },
 }
