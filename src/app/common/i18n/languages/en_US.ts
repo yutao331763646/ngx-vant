11f1d9a47cdec66e5572868d633d5765c7d70463
yutao <yutao0818@vip.qq.com>
2021-08-13 14:09:56 +0800

    feat(button doc i18n):doing

--- src/app/common/i18n/languages/en_US.ts
+++ src/app/common/i18n/languages/en_US.ts
@@ -4,10 +4,15 @@ export default {
     // locale: 'en_US',
     ...en_US,
     button: {
+        type:'Type',
         primary: 'Primary',
         info: 'Info',
         default: 'Default',
         danger: 'Danger',
         warning: 'Warning',
+
+        plain:'Plain',
+        hairline:'Hairline',
+        hairlineBtn:'Hairline',
     },
 }
--- src/app/common/i18n/languages/zh_CN.ts
+++ src/app/common/i18n/languages/zh_CN.ts
@@ -4,10 +4,16 @@ export default {
     // locale: 'zh_CN',
     ...zh_CN,
     button: {
+        type:'按钮类型',
         primary: '主要按钮',
         info: '信息按钮',
         default: '默认按钮',
         danger: '危险按钮',
         warning: '警告按钮',
+
+        plain:'朴素按钮',
+        hairline:'细边框',
+        hairlineBtn:'细边框按钮',
+
     },
 }
