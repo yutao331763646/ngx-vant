baa6fb3983f59be752a02e3f5f21d002f87c2625
yutao <yutao0818@vip.qq.com>
2021-08-14 16:13:56 +0800

    feat(theme doc):doing

--- src/app/page/en_US/theme/theme.component.ts
+++ src/app/page/en_US/theme/theme.component.ts
@@ -2,20 +2,26 @@ import { Component, Input, OnInit } from '@angular/core';
 import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
-  selector: 'app-theme',
-  templateUrl: './theme.component.html',
-  styleUrls: ['./theme.component.less']
+    selector: 'app-theme',
+    templateUrl: './theme.component.html',
+    styleUrls: ['./theme.component.less']
 })
 export class ThemeComponent implements OnInit {
+    private _readMe: HTMLElement | string = '';
     @Input() set readMe(readMe: HTMLElement | string) {
         this._readMe = readMe;
     }
     get readMe() {
         return cardWrapper(this._readMe);
     }
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+
+    ngOnInit() {
+        this.setReadMe()
+    }
+    setReadMe() {
+        this.readMe = require(`!html-loader!markdown-loader!./theme.zh-CN.md`).default;
+    }
 
 }
