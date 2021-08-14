c9f9ec22078b52c9c9f269029026056e6525156b
yutao <yutao0818@vip.qq.com>
2021-08-14 16:13:10 +0800

    feat(home doc):doing

--- src/app/page/en_US/theme/theme.component.ts
+++ src/app/page/en_US/theme/theme.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
   selector: 'app-theme',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./theme.component.less']
 })
 export class ThemeComponent implements OnInit {
-
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
   constructor() { }
 
   ngOnInit() {
