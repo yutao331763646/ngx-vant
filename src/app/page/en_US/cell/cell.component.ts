9ea9fd02d4fe76bbc5f0c48746b3c00e8bb3cec5
yutao <yutao0818@vip.qq.com>
2021-08-15 23:39:43 +0800

    feat(cell doc):doing

--- src/app/page/en_US/cell/cell.component.ts
+++ src/app/page/en_US/cell/cell.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cell.component.less']
 })
 export class CellComponent implements OnInit {
+    private _readMe: HTMLElement | string = '';
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
 
   constructor() { }
 
