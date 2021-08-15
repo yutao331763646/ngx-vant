33b490cf381ffecf739907b51d139f0d9d1ca709
yutao <yutao0818@vip.qq.com>
2021-08-15 23:40:05 +0800

    feat(cell doc):doing

--- src/app/page/en_US/cell/cell.component.ts
+++ src/app/page/en_US/cell/cell.component.ts
@@ -2,9 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
-  selector: 'app-cell',
-  templateUrl: './cell.component.html',
-  styleUrls: ['./cell.component.less']
+    selector: 'app-cell',
+    templateUrl: './cell.component.html',
+    styleUrls: ['./cell.component.less']
 })
 export class CellComponent implements OnInit {
     private _readMe: HTMLElement | string = '';
@@ -15,9 +15,13 @@ export class CellComponent implements OnInit {
         return cardWrapper(this._readMe);
     }
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.setReadMe()
+    }
+    setReadMe() {
+        this.readMe = require(`!html-loader!markdown-loader!./README.zh-CN.md`).default;
+    }
 
 }
