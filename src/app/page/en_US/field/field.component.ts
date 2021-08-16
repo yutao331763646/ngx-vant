68f54852d581b5dbaf422610893f193005d0d92d
yutao <yutao0818@vip.qq.com>
2021-08-16 20:06:40 +0800

    feat(field doc):doing

--- src/app/page/en_US/field/field.component.ts
+++ src/app/page/en_US/field/field.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./field.component.less']
 })
 export class FieldComponent implements OnInit {
-
+    private _readMe: HTMLElement | string = '';
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
   constructor() { }
 
   ngOnInit() {
