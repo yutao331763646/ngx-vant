2597dec9744f652245cf1d177e7e8589eb5f91a1
yutao <yutao0818@vip.qq.com>
2021-08-16 20:16:33 +0800

    feat(field doc):doing

--- src/app/page/en_US/field/field.component.ts
+++ src/app/page/en_US/field/field.component.ts
@@ -20,7 +20,7 @@ export class FieldComponent implements OnInit {
         this.setReadMe()
     }
     setReadMe() {
-        this.readMe = require(`!html-loader!markdown-loader!./README.zh-CN.md`).default;
+        this.readMe = require(`!html-loader!markdown-loader!./README.md`).default;
     }
 
 }
