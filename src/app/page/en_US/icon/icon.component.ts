a6c95dbe26ef211e6aaf2d4496733ddf64d7638f
yutao <yutao0818@vip.qq.com>
2021-08-17 19:46:13 +0800

    feat(icon en doc):doing

--- src/app/page/en_US/icon/icon.component.ts
+++ src/app/page/en_US/icon/icon.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon.component.less']
 })
 export class IconComponent implements OnInit {
-
+    private _readMe: HTMLElement | string = '';
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
   constructor() { }
 
   ngOnInit() {
