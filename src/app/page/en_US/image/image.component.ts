653ba710d8335ff4adc463f1a54536c9efc25dd1
yutao <yutao0818@vip.qq.com>
2021-08-18 20:26:44 +0800

    feat(image en doc):doing

---
+++ src/app/page/en_US/image/image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.less']
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
