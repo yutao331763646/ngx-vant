1bd57af6ab14c61c9af084b2e6cb8eefcf033785
yutao <yutao0818@vip.qq.com>
2021-08-18 20:33:22 +0800

    feat(image en doc):doing

--- src/app/page/en_US/image/image.component.ts
+++ src/app/page/en_US/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
