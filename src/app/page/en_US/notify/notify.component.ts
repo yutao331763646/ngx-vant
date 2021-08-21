b3486c6cca4ab34fd1d0a2124280109e03cf3d39
yutao <yutao0818@vip.qq.com>
2021-08-21 14:50:25 +0800

    feat(notify en doc):doing

--- src/app/page/en_US/notify/notify.component.ts
+++ src/app/page/en_US/notify/notify.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notify.component.less']
 })
 export class NotifyComponent implements OnInit {
-
+    private _readMe: HTMLElement | string = '';
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
   constructor() { }
 
   ngOnInit() {
