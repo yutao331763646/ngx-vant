7bc71e936cf10e4c3edb94a3e090a7b0c59523d2
yutao <yutao0818@vip.qq.com>
2021-08-22 19:14:27 +0800

    feat(picker en doc):doing

--- src/app/page/en_US/picker/picker.component.ts
+++ src/app/page/en_US/picker/picker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { cardWrapper } from 'src/app/common/card-wrapper';
 
 @Component({
   selector: 'app-picker',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picker.component.less']
 })
 export class PickerComponent implements OnInit {
-
+    private _readMe: HTMLElement | string = '';
+    @Input() set readMe(readMe: HTMLElement | string) {
+        this._readMe = readMe;
+    }
+    get readMe() {
+        return cardWrapper(this._readMe);
+    }
   constructor() { }
 
   ngOnInit() {
-  }
-
+    this.setReadMe()
+}
+setReadMe() {
+    this.readMe = require(`!html-loader!markdown-loader!./README.md`).default;
+}
 }
